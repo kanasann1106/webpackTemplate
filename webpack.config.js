1a904c4600da3d68f9452c74395b7e99700ba4e6
Haebaru Kana <kana.sakura1106@gmail.com>
2020-02-11 23:53:35 +0900

    コーディングテンプレート更新

--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,177 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin'); // js,css別々で出力
-const TerserPlugin = require('terser-webpack-plugin'); // js minify
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin'); // css minify
+const AutoPrefixer = require('autoprefixer');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const globule = require('globule');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = (env, argv) => ({
-  devServer: {
-    contentBase: path.join(__dirname, './public')
+const app = {
+  entry: {
+    app: [
+      './src/js/app.js',
+      './src/sass/style.scss',
+    ],
   },
-  // メインとなるJavaScriptファイル（エントリーポイント）
-  entry: './src/js/index.js',
-  // ファイルの出力設定
   output: {
-    path: path.join(__dirname, './public'),
-    // 出力ファイル名
-    filename: './assets/js/bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js',
+    publicPath: '/',
   },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        extractComments: 'all'
-      }),
-      new OptimizeCSSAssetsPlugin({
-        discardComments: { removeAll: true }
-      })
-    ]
+  devServer: {
+    contentBase: './dist/html/',
+    watchContentBase: true,
+    port: 3000,
+    open: true,
   },
   module: {
     rules: [
       {
-        // JSファイルの読み込みとコンパイル
-        test: /\.js$/,
+        test: /\.pug$/,
+        exclude: /node_modules/,
         use: [
           {
-            // Babel を利用する
-            loader: 'babel-loader',
-            // Babel のオプションを指定する
+            loader: 'pug-loader',
             options: {
-              presets: [
-                // プリセットを指定することで、ES2019 を ES5 に変換
-                '@babel/preset-env'
-              ]
-            }
-          }
-        ]
+              pretty: true,
+              root: path.resolve(__dirname, 'src/pug'),
+            },
+          },
+        ],
       },
-      // Sassファイルの読み込みとコンパイル
       {
-        test: /\.scss/, // 対象となるファイルの拡張子
-        use: ExtractTextPlugin.extract({
-          use: [
-            // CSSをバンドルするための機能
-            {
-              loader: 'css-loader',
-              options: {
-                // オプションでCSS内のurl()メソッドの取り込みを禁止する
-                url: false,
-                // ソースマップの利用有無
-                sourceMap: true,
-
-                // 0 => no loaders (default);
-                // 1 => postcss-loader;
-                // 2 => postcss-loader, sass-loader
-                importLoaders: 2
-              }
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              sourceMap: true,
             },
-            // PostCSSのための設定
-            {
-              loader: 'postcss-loader',
-              options: {
-                // PostCSS側でもソースマップを有効にする
-                sourceMap: true,
-                plugins: [
-                  // Autoprefixerを有効化
-                  // ベンダープレフィックスを自動付与する
-                  require('autoprefixer')({
-                    grid: true,
-                    overrideBrowserslist: ['last 2 versions']
-                  })
-                ]
-              }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                AutoPrefixer(),
+              ],
             },
-            'sass-loader'
-          ]
-        })
-      }
-    ]
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)(\?[a-z0-9=.]+)?$/,
+        loader: 'url-loader',
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf|svg)(\?\S*)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
   },
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin(),
+      new UglifyJsPlugin(),
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
+  devtool: 'source-map',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, 'src/assets/images/'),
+        to: path.resolve(__dirname, 'dist/assets/images/'),
+      },
+      {
+        from: path.resolve(__dirname, 'src/assets/webfonts/'),
+        to: path.resolve(__dirname, 'dist/assets/webfonts/'),
+      },
+      {
+        from: path.resolve(__dirname, 'src/assets/media/'),
+        to: path.resolve(__dirname, 'dist/assets/media/'),
+      },
+    ]),
+    new ImageminPlugin({
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      pngquant: {
+        quality: '95-100',
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/style.css',
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+  ],
+};
 
-  plugins: [new ExtractTextPlugin('./assets/css/style.css')]
+// pugファイルをglobuleで探す
+const pugFiles = globule.find(
+  './src/pug/**/*.pug', {
+    ignore: [
+      './src/pug/**/_*/*.pug',
+    ],
+  },
+);
+
+// 探してきたpugファイルをhtmlに変換してpublicのhtmlに移動
+pugFiles.forEach((pugFile) => {
+  const fileName = pugFile.replace('./src/pug/', 'html/').replace('.pug', '.html');
+  app.plugins.push(
+    // HTMLファイル出力
+    new HtmlWebpackPlugin({
+      inject: false, // 自動でjsとcssファイルの読み込みを書かない
+      filename: `${fileName}`,
+      template: pugFile,
+    }),
+  );
 });
+
+module.exports = app;
